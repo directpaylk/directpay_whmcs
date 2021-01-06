f291b3cc13806f2ba49bcc0c69df9fe9442f8cb0
SupunSSW <ssadeepa7@gmail.com>
2021-01-06 18:01:03 +0530

    Changed response logging

--- modules/gateways/callback/directpay.php
+++ modules/gateways/callback/directpay.php
@@ -47,6 +47,7 @@ logActivity('$postBody_raw: ' . gettype($postBody_raw));
 logActivity('$postBody_raw: ' . sizeof($postBody_raw));
 logActivity('$postBody_raw: ' . json_encode($postBody_raw));
 logActivity('$postBody_raw: ' . $postBody_raw);
+logActivity('$postBody_raw: ' . base64_decode($postBody_raw));
 foreach ($postBody_raw as $res) {
     logActivity('$postBody_raw: IN-FOREACH 1 ' . gettype($res));
     logActivity('$postBody_raw: IN-FOREACH 2 ' . $res);
